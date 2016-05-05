a0713abcf64f35c1bace56064895015d88f7958f
Justin Madsen <justinmadsen4@gmail.com>
2016-05-04 19:26:49 -0600

    Initial Commit

---
+++ toodles/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  toodles
+//
+//  Created by Justin Madsen on 5/4/16.
+//  Copyright © 2016 mbv. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
